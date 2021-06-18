e47565d7216f132c371bf7a9db93a70a058bcb35
mau <mauriciorj76@gmail.com>
2021-06-18 09:33:14 -0600

    Primera

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  //Entrada
+  entry: "./src/index.js",
+
+  //Salida de archivos cuando se compile
+  output: {
+    path: path.resolve(__dirname, dist),
+    filename: "bundle.js",
+  },
+
+  //Resolver extenciones del Proyecto
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  //REglas para Proyecto
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+
+      //Regla para trabajar con HTML
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
+  },
+
+  //Aqui estan los Plugins
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
