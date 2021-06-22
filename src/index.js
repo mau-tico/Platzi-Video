a61aad1d94a39a27e893b766e1899a944d16ab5f
mau <mauriciorj76@gmail.com>
2021-06-22 09:10:27 -0600

    Undecimo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import HelloWorld from "./components/HelloWorld";
+import App from "./containers/App";
+import "./assets/styles/App.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HelloWorld />
+    <App />
   </React.StrictMode>,
   document.getElementById("app")
 );
