a8352133150a53b490232cf12b47e8f5c97a6d32
mau <mauriciorj76@gmail.com>
2021-06-23 08:29:22 -0600

    Decimo sexto

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,15 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "../assets/styles/components/CarouselItem.scss";
 import playIcon from "../assets/static/play-icon.png";
 import plusIcon from "../assets/static/plus-icon.png";
 
-const CarouselItem = ({cover, title, year, contentRating, duration}) => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
   <div className="carousel-item">
-    <img
-      className="carousel-item__img"
-      src={cover}
-      alt={title}
-    />
+    <img className="carousel-item__img" src={cover} alt={title} />
     <div className="carousel-item__details">
       <div>
         <img
@@ -25,10 +22,18 @@ const CarouselItem = ({cover, title, year, contentRating, duration}) => (
       </div>
       <p className="carousel-item__details--title">{title}</p>
       <p className="carousel-item__details--subtitle">
-      {`${year} ${contentRating} ${duration}`}
+        {`${year} ${contentRating} ${duration}`}
       </p>
     </div>
   </div>
 );
+
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+};
 
 export default CarouselItem;
