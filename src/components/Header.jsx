9e887864b4108194bb6b05e8ccc4a05ab26f3c94
mau <mauriciorj76@gmail.com>
2021-06-29 19:42:43 -0600

    Linux4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import gravatar from "../utils/gravatar";
+import { logoutRequest } from "../actions";
 import "../assets/styles/components/Header.scss";
 import logo from "../assets/static/logo-platzi-video-BW2.png";
 import userIcon from "../assets/static/user-icon.png";
@@ -9,6 +10,10 @@ import userIcon from "../assets/static/user-icon.png";
 const Header = (props) => {
   const { user } = props;
   const hashUser = Object.keys(user).length > 0;
+
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
 
   return (
     <header className="header">
@@ -26,12 +31,23 @@ const Header = (props) => {
           <p>Perfil</p>
         </div>
         <ul>
-          <li>
-            <a href="/">Cuenta</a>
-          </li>
-          <li>
-            <Link to="/login">Iniciar Sesión</Link>
-          </li>
+          {hashUser ? (
+            <li>
+              <a href="/">{user.name}</a>
+            </li>
+          ) : null}
+
+          {hashUser ? (
+            <li>
+              <a href="#logout" onClick={handleLogout}>
+                Cerrar Sesión
+              </a>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Iniciar Sesión</Link>
+            </li>
+          )}
         </ul>
       </div>
     </header>
@@ -44,5 +60,9 @@ const mapStateToProps = (state) => {
   };
 };
 
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
 //export default Header;
-export default connect(mapStateToProps, null)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
